9149ce4ac8f3e4a8e22c0b26732bfe1c687b5c81
duluman <cristian.duluman@gmail.com>
2020-09-22 22:08:37 +0300

    ready for deploy

--- users/models.py
+++ users/models.py
@@ -87,7 +87,7 @@ class EnContactApp(models.Model):
 
 
 class RoNewsletter(models.Model):
-    email = models.EmailField(unique=True, null=False, max_length=255)
+    email = models.EmailField(unique=False, null=False, max_length=255)
 
     def __str__(self):
         return self.email
